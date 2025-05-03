5ddf5a5e950b543c6e7c566d010230b8b87174ce
Codie Newark <a13x231mcsl@gmail.com>
2025-05-03 01:07:46 +0100

    Updated publish script.

--- publish.py
+++ publish.py
@@ -12,36 +12,11 @@ print(f"current version = {current_version}")
 print("please enter the new version")
 new_version = input("new version = ")
 subprocess.call(['cargo', 'version-util', 'set-version', new_version])
+subprocess.call(['npm', 'version', new_version], cwd='./blf_lsp-npm')
+subprocess.call(['napi', 'version', '--config', '../blf_lsp/config.json'], cwd='./blf_lsp-npm')
 
-print("blf_lib-derivable: publishing")
-try:
-    subprocess.call(['cargo', 'publish', '-p', 'blf_lib-derivable', '--allow-dirty'])
-except:
-    print("Failed to publish blf_lib-derivable")
-
-print("blf_lib-derive: publishing")
-try:
-    subprocess.Popen(f'cargo upgrade -p blf_lib-derivable@{new_version} --manifest-path blf_lib-derive/Cargo.toml --pinned')
-    subprocess.call(['cargo', 'publish', '-p', 'blf_lib-derive', '--allow-dirty'])
-except:
-    print("Failed to publish blf_lib-derive")
-
-print("blf_lib: publishing")
-try:
-    subprocess.Popen(f'cargo upgrade -p blf_lib-derivable@{new_version} -p blf_lib-derive@{new_version} --manifest-path blf_lib/Cargo.toml --pinned')
-    subprocess.call(['cargo', 'publish', '-p', 'blf_lib', '--allow-dirty'])
-except:
-    print("Failed to publish blf_lib")
-
-print("blf_cli: publishing")
-try:
-    subprocess.Popen(f'cargo upgrade -p blf_lib@{new_version} --manifest-path blf_cli/Cargo.toml --pinned')
-    subprocess.call(['cargo', 'publish', '-p', 'blf_lib', '--allow-dirty'])
-except:
-    print("Failed to publish blf_cli")
-
-subprocess.call(['git', 'add', 'Cargo.toml'])
-subprocess.call(['git', 'commit', '-m', f'"Publish {new_version}\"'])
-subprocess.call(['git', 'tag', f'v{new_version}'])
+subprocess.call(['git', 'add', 'Cargo.toml', '**/package.json'])
+subprocess.call(['git', 'commit', '-m', f'"v{new_version}\"'])
+subprocess.call(['git', 'tag', f'{new_version}'])
 subprocess.call(['git', 'push'])
-subprocess.call(['git', 'push', 'origin', f'v{new_version}'])
+subprocess.call(['git', 'push', 'origin', f'{new_version}'])
