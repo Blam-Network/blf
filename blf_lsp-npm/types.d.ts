ecade7ab21961d2a6602a5a984da22a11b19f09f
Codie Newark <a13x231mcsl@gmail.com>
2025-05-10 23:29:12 +0100

    missed from last commit

---
+++ blf_lsp-npm/types.d.ts
@@ -0,0 +1,5 @@
+export declare type StaticString = string;
+export declare type StaticWcharString = string;
+export declare type StaticArray<T> = T[];
+export declare type s_bool = boolean;
+export declare type time32_t = Date;
