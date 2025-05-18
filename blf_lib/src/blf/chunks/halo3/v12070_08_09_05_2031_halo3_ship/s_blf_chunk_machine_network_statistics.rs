76c47cf364ac447ceb3428e8d0179f32c0fcb4a7
Codie Newark <a13x231mcsl@gmail.com>
2025-05-18 13:59:50 +0100

    napi support for recent players, machine stats

--- blf_lib/src/blf/chunks/halo3/v12070_08_09_05_2031_halo3_ship/s_blf_chunk_machine_network_statistics.rs
+++ blf_lib/src/blf/chunks/halo3/v12070_08_09_05_2031_halo3_ship/s_blf_chunk_machine_network_statistics.rs
@@ -3,14 +3,17 @@ use serde::{Deserialize, Serialize};
 use blf_lib::types::array::StaticArray;
 use blf_lib_derivable::blf::chunks::BlfChunkHooks;
 use blf_lib_derive::BlfChunk;
+#[cfg(feature = "napi")]
+use napi_derive::napi;
 
 #[binrw]
+#[cfg_attr(feature = "napi", napi(object, namespace = "halo3_12070_08_09_05_2031_halo3_ship"))]
 #[derive(BlfChunk,PartialEq,Debug,Clone,Serialize,Deserialize,Default)]
 #[Header("funs", 1.1)]
 #[brw(big)]
 pub struct s_blf_chunk_machine_network_statistics {
     // need to map this whole ass struct
-    data: StaticArray<u8, 0xC0>,
+    pub data: StaticArray<u8, 0xC0>,
 }
 
 impl BlfChunkHooks for s_blf_chunk_machine_network_statistics {}
--- blf_lib/src/blf/chunks/halo3/v12070_08_09_05_2031_halo3_ship/s_blf_chunk_recent_players.rs
+++ blf_lib/src/blf/chunks/halo3/v12070_08_09_05_2031_halo3_ship/s_blf_chunk_recent_players.rs
@@ -4,12 +4,15 @@ use serde::{Deserialize, Serialize};
 use blf_lib_derivable::blf::chunks::BlfChunkHooks;
 use blf_lib_derive::BlfChunk;
 use crate::types::u64::Unsigned64;
+#[cfg(feature = "napi")]
+use napi_derive::napi;
 
 // const k_recent_players_max_count: usize = -1;
 #[binrw]
 #[derive(BlfChunk,Default,PartialEq,Debug,Clone,Serialize,Deserialize)]
 #[Header("furp", 2.1)]
 #[brw(big)]
+#[cfg_attr(feature = "napi", napi(object, namespace = "halo3_12070_08_09_05_2031_halo3_ship"))]
 pub struct s_blf_chunk_recent_players
 {
     #[bw(try_calc(u32::try_from(players.len())))]
@@ -18,12 +21,13 @@ pub struct s_blf_chunk_recent_players
     pub players: Vec<s_blf_chunk_recent_players_player> // UTF bytes,
 }
 
+#[cfg_attr(feature = "napi", napi(object, namespace = "halo3_12070_08_09_05_2031_halo3_ship"))]
 #[derive(Default,PartialEq,Debug,Clone,Serialize,Deserialize,BinRead,BinWrite)]
 pub struct s_blf_chunk_recent_players_player {
     // We had this down as a u16 in the old blf tool.
-    unknown1: u8,
-    unknown2: u8,
-    xuid: Unsigned64,
+    pub unknown1: u8,
+    pub unknown2: u8,
+    pub xuid: Unsigned64,
 }
 
 impl BlfChunkHooks for s_blf_chunk_recent_players {
