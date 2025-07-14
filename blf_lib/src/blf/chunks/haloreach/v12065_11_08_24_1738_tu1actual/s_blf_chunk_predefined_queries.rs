f0a6c6f34ddbcfc49341a635864ff9388a559edf
Codie Newark <a13x231mcsl@gmail.com>
2025-07-15 00:05:59 +0100

    tidied reach fpre 1.1

--- blf_lib/src/blf/chunks/haloreach/v12065_11_08_24_1738_tu1actual/s_blf_chunk_predefined_queries.rs
+++ blf_lib/src/blf/chunks/haloreach/v12065_11_08_24_1738_tu1actual/s_blf_chunk_predefined_queries.rs
@@ -17,8 +17,6 @@ use blf_lib_derivable::blf::chunks::BlfChunkHooks;
 use blf_lib_derive::{BlfChunk, TestSize};
 use crate::types::numbers::Float32;
 
-pub const k_manifest_maximum_maps_count: usize = 16;
-
 #[binrw]
 #[derive(BlfChunk,Default,PartialEq,Debug,Clone,Serialize,Deserialize)]
 #[Header("fpre", 1.1)]
@@ -28,12 +26,12 @@ pub struct s_blf_chunk_predefined_queries
     #[bw(try_calc(u32::try_from(queries.len())))]
     query_count: u32,
     #[br(count = query_count)]
-    pub queries: Vec<s_blf_chunk_megalo_category>,
+    pub queries: Vec<s_blf_chunk_predefined_query>,
 }
 
 impl BlfChunkHooks for s_blf_chunk_predefined_queries {}
 #[derive(Clone, Default, PartialEq, Debug, Serialize, Deserialize, BinRead, BinWrite)]
-pub struct s_blf_chunk_megalo_category {
+pub struct s_blf_chunk_predefined_query {
     pub unknown1: u32,
     pub unknown2: u32,
     pub unknown3: u32,
