f75cdceabbaa8d0cd33f513ed1f97ab77b856465
Codie Newark <a13x231mcsl@gmail.com>
2025-08-06 12:45:17 +0200

    added napi support for omaha arhs

--- blf_lib/src/blf/chunks/haloreach/v12065_11_08_24_1738_tu1actual/s_blf_chunk_arena_hopper_stats.rs
+++ blf_lib/src/blf/chunks/haloreach/v12065_11_08_24_1738_tu1actual/s_blf_chunk_arena_hopper_stats.rs
@@ -3,14 +3,17 @@ use blf_lib::blf::chunks::BlfChunkHooks;
 use blf_lib::BlfChunk;
 use serde::{Deserialize, Serialize};
 use blf_lib::types::array::StaticArray;
+#[cfg(feature = "napi")]
+use napi_derive::napi;
 
 #[binrw]
 #[derive(BlfChunk,PartialEq,Debug,Clone,Serialize,Deserialize,Default)]
 #[Header("arhs", 3.1)]
 #[brw(big)]
+#[cfg_attr(feature = "napi", napi(object, namespace = "haloreach_12065_11_08_24_1738_tu1actual"))]
 pub struct s_blf_chunk_arena_hopper_stats {
     // TODO: Map
-    data: StaticArray<u8, 0x16>,
+    pub data: StaticArray<u8, 0x16>,
 }
 
 impl BlfChunkHooks for s_blf_chunk_arena_hopper_stats {}
