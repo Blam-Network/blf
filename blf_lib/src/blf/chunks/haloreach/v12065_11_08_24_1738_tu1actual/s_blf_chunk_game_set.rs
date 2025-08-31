ed985290ce4b6286a08da0536f26b1e6a1fdb378
Codie Newark <a13x231mcsl@gmail.com>
2025-08-31 16:01:49 +0900

    mapped more of reach's game set file

--- blf_lib/src/blf/chunks/haloreach/v12065_11_08_24_1738_tu1actual/s_blf_chunk_game_set.rs
+++ blf_lib/src/blf/chunks/haloreach/v12065_11_08_24_1738_tu1actual/s_blf_chunk_game_set.rs
@@ -113,26 +113,39 @@ impl BinWrite for s_blf_chunk_game_set {
 
 #[derive(Clone, Default, PartialEq, Debug, Copy, Serialize, Deserialize)]
 #[binrw]
+pub struct s_game_set_entry_campaign_and_survival_data {
+    pub dword00: Float32,
+    pub dword04: u32,
+    pub dword08: u32,
+    pub dword0C: u32,
+}
+
+#[derive(Clone, Default, PartialEq, Debug, Copy, Serialize, Deserialize)]
+#[binrw]
+pub struct s_game_set_entry_replicated_data {
+    #[brw(pad_after = 1)]
+    pub unknown00: Bool,
+    pub unknown02: u16,
+    pub unknown04: u32,
+    pub unknown08: Float32,
+    pub unknown0C: Float32,
+    pub unknown10: Float32,
+    #[brw(pad_after = 3)]
+    pub unknown14: Bool,
+}
+
+#[derive(Clone, Default, PartialEq, Debug, Copy, Serialize, Deserialize)]
+#[binrw]
 pub struct s_game_set_entry {
-    pub dword0: u32,
-    pub dword4: u32,
-    pub dword8: u32,
+    pub weight: u32,
+    pub minimum_player_count: u32,
+    pub maximum_player_count: u32,
     pub dwordc: u32,
     pub dword10: u32,
     pub dword14: u32,
     pub dword18: u32,
-    pub dword1c: u32,
-    pub dword20: u32,
-    pub dword24: u32,
-    pub dword28: u32,
-    pub gap2c: u16,
-    pub word2e: u16,
-    pub dword30: u32,
-    pub dword34: u32,
-    pub float38: Float32,
-    pub float3c: Float32,
-    #[brw(pad_after = 3)]
-    pub byte40: u8,
+    pub campaign_and_survival_data: s_game_set_entry_campaign_and_survival_data,
+    pub replicated_data:s_game_set_entry_replicated_data,
     // #[serde(skip_serializing,skip_deserializing)]
     pub map_id: u32,
     #[serde(skip_serializing,skip_deserializing)]
@@ -150,6 +163,6 @@ pub struct s_game_set_entry {
     #[serde(skip_serializing_if = "StaticString::is_empty", default)]
     pub map_variant_file_name: StaticString<32>,
     #[serde(skip_serializing,skip_deserializing)]
+    #[brw(pad_after = 2)]
     pub map_variant_hash: s_network_http_request_hash,
-    pub unknown3: u16,
 }
