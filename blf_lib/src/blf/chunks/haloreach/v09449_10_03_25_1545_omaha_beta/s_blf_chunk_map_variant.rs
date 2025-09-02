29a729022392644be499b876e18fb8f27c8db258
Codie Newark <a13x231mcsl@gmail.com>
2025-09-03 04:03:02 +0900

    added missing file

---
+++ blf_lib/src/blf/chunks/haloreach/v09449_10_03_25_1545_omaha_beta/s_blf_chunk_map_variant.rs
@@ -0,0 +1,21 @@
+use binrw::binrw;
+use serde::{Deserialize, Serialize};
+use blf_lib::blam::common::memory::secure_signature::s_network_http_request_hash;
+use blf_lib_derivable::blf::chunks::BlfChunkHooks;
+use blf_lib_derive::BlfChunk;
+
+#[binrw]
+#[derive(BlfChunk,PartialEq,Debug,Clone,Serialize,Deserialize)]
+#[Header("mvar", 19.1)]
+#[brw(big)]
+#[derive(Default)]
+pub struct s_blf_chunk_map_variant
+{
+    pub hash: s_network_http_request_hash,
+    pub data_size: u32,
+    #[br(count = data_size)]
+    pub data: Vec<u8>,
+}
+
+
+impl BlfChunkHooks for s_blf_chunk_map_variant {}
