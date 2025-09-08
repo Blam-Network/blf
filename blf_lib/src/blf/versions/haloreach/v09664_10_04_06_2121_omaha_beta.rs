4beaf3be8a7377f07f02d082db3ab9d28494650e
Codie Newark <a13x231mcsl@gmail.com>
2025-09-08 13:37:13 +0900

    omaha 09664 version file

---
+++ blf_lib/src/blf/versions/haloreach/v09664_10_04_06_2121_omaha_beta.rs
@@ -0,0 +1,35 @@
+use blf_lib_derive::TitleAndBuild;
+use crate::blf::chunks::halo3;
+use crate::blf::chunks::haloreach;
+use crate::blf::chunks::halo3odst;
+
+pub use halo3::v12070_08_09_05_2031_halo3_ship::s_blf_chunk_start_of_file::*;
+pub use halo3::v12070_08_09_05_2031_halo3_ship::s_blf_chunk_end_of_file::*;
+pub use halo3::v12070_08_09_05_2031_halo3_ship::s_blf_chunk_map_manifest::*;
+pub use halo3::v12070_08_09_05_2031_halo3_ship::s_blf_chunk_hopper_description_table::*;
+pub use halo3::v12070_08_09_05_2031_halo3_ship::s_blf_chunk_online_file_manifest::*;
+pub use halo3::v12070_08_09_05_2031_halo3_ship::s_blf_chunk_banhammer_messages::*;
+pub use halo3::v12070_08_09_05_2031_halo3_ship::s_blf_chunk_matchmaking_tips::*;
+pub use halo3odst::v13895_09_04_27_2201_atlas_release::s_blf_chunk_author::*;
+pub use haloreach::v09730_10_04_09_1309_omaha_delta::s_blf_chunk_hopper_configuration_table::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_player_data::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_arena_hopper_stats::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_player_heartbeat_response::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_daily_challenges::*;
+pub use haloreach::v09730_10_04_09_1309_omaha_delta::s_blf_chunk_rewards_persistance::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_service_record::*;
+pub use haloreach::v09730_10_04_09_1309_omaha_delta::s_blf_chunk_game_set::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_nag_message::*;
+pub use haloreach::v09730_10_04_09_1309_omaha_delta::s_blf_chunk_network_configuration::*;
+pub use haloreach::v09730_10_04_09_1309_omaha_delta::s_blf_chunk_map_variant::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_content_header::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_matchmaking_game_variant::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_dlc_map_manifest::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_megalo_categories::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_predefined_queries::*;
+pub use haloreach::v12065_11_08_24_1738_tu1actual::s_blf_chunk_matchmaking_hopper_statistics::*;
+
+#[derive(TitleAndBuild)]
+#[Title("Halo: Reach")]
+#[Build("09664.10.04.06.2121.omaha_beta")]
+pub struct v09664_10_04_06_2121_omaha_beta {}
