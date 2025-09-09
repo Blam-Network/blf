3ae89165452a07fa27e7884c373bc1404061fd02
Codie Newark <a13x231mcsl@gmail.com>
2025-09-10 00:39:37 +0900

    rework string table for consistency with bungie behaviour

--- blf_lib/src/blam/haloreach/v12065_11_08_24_1738_tu1actual/game/string_table.rs
+++ blf_lib/src/blam/haloreach/v12065_11_08_24_1738_tu1actual/game/string_table.rs
@@ -7,6 +7,7 @@ use blf_lib_derivable::result::BLFLibResult;
 use crate::assert_ok;
 use crate::blam::common::memory::data_compress::{runtime_data_compress, runtime_data_decompress};
 use crate::io::bitstream::{c_bitstream_reader, c_bitstream_writer};
+use crate::io::bitstream::e_bitstream_byte_order::_bitstream_byte_order_big_endian;
 
 #[derive(Default, Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct c_single_language_string_table<
@@ -117,8 +118,7 @@ pub struct c_string_table<
     const buffer_size_bit_length: usize,
     const count_bit_length: usize,
 > {
-    strings: StaticArray<Vec<String>, k_language_count>,
-
+    strings: StaticArray<Vec<Option<String>>, k_language_count>,
 }
 
 impl<
@@ -143,11 +143,14 @@ c_string_table<
             return Ok(());
         }
 
-        let mut offsets = vec![vec![0; string_count]; k_language_count];
+        let mut offsets = vec![vec![0i16; string_count]; k_language_count];
         for j in 0..string_count {
             for i in 0..k_language_count {
                 if bitstream.read_bool("exists")? {
                     offsets[i][j] = bitstream.read_integer("index", offset_bit_length)?;
+                }
+                else {
+                    offsets[i][j] = -1;
                 }
             }
         }
@@ -168,11 +171,15 @@ c_string_table<
         let mut string_reader = Cursor::new(string_data);
         for (language_index, language_offsets) in offsets.iter().enumerate() {
             for &offset in language_offsets {
-                string_reader.seek(SeekFrom::Start(offset))?;
+                if offset == -1 {
+                    self.strings[language_index].push(None);
+                    continue;
+                }
+                assert_ok!(offset >= 0);
+                let offset = offset as u64;
 
-                self.strings.get_mut()[language_index].push(NullString::read(&mut string_reader)?.to_string());
-
                 string_reader.seek(SeekFrom::Start(offset))?;
+                self.strings[language_index].push(Some(NullString::read(&mut string_reader)?.to_string()));
             }
         }
 
@@ -187,25 +194,59 @@ c_string_table<
             return Ok(());
         }
 
-        let mut language_offsets = vec![0u16; k_language_count];
+        let mut string_writer = c_bitstream_writer::new(
+            k_language_count * max_string_count * max_string_length,
+            _bitstream_byte_order_big_endian
+        );
+        let mut offset = 0u16;
+        string_writer.begin_writing();
         for i in 0..self.strings[0].len() {
+            // If the string is the same in every language, we consider it non-localized
+            // and only write it once.
+            // This is how Bungie handles duplicates.
+            let mut deduplicate = true;
+            let string = self.strings[0][i].clone();
+
+            if string.is_none() {
+                deduplicate = false;
+            }
+
+            for language_index in 1..k_language_count {
+                if string != self.strings[language_index][i] {
+                    deduplicate = false;
+                    break;
+                }
+            }
+
             for language_index in 0..k_language_count {
-                bitstream.write_bool(true)?;
-                bitstream.write_integer(language_offsets[language_index], offset_bit_length)?;
-                language_offsets[language_index] += self.strings[language_index][i].len() as u16;
+                let string = &self.strings[language_index][i];
+                match string {
+                    None => {
+                        bitstream.write_bool(false)?;
+                        continue;
+                    }
+                    Some(string) => {
+                        bitstream.write_bool(true)?;
+                        bitstream.write_integer(offset, offset_bit_length)?;
+
+                        if !deduplicate {
+                            string_writer.write_string_utf8(string, max_string_length as u32)?;
+                            offset += string.len() as u16 + 1;
+                        }
+                    }
+                }
+            }
+
+            if deduplicate {
+                let string = string.unwrap();
+                string_writer.write_string_utf8(&string, max_string_length as u32)?;
+                offset += string.len() as u16 + 1;
             }
         }
 
-        let mut buffer = Vec::<u8>::new();
+        string_writer.finish_writing();
+        let buffer = string_writer.get_data()?;
         let mut compressed_buffer = Vec::with_capacity(buffer.len());
-        let mut string_writer = Cursor::new(&mut buffer);
-        for language_strings in self.strings.get().iter() {
-            language_strings
-                .iter()
-                .map(|string| NullString::from(string.to_string()))
-                .collect::<Vec<_>>()
-                .write_options(&mut string_writer, bitstream.get_byte_order().into(), ())?;
-        }
         runtime_data_compress(&buffer, &mut compressed_buffer, bitstream.get_byte_order())?;
 
         bitstream.write_integer(buffer.len() as u32, buffer_size_bit_length)?;
