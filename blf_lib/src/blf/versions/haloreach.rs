2847b84b5948cf422df76795149230030b4ca7fe
Codie Newark <a13x231mcsl@gmail.com>
2025-09-14 16:25:45 +0900

    omaha dlc_1_ship version file

--- blf_lib/src/blf/versions/haloreach.rs
+++ blf_lib/src/blf/versions/haloreach.rs
@@ -1,8 +1,9 @@
 pub mod v12065_11_08_24_1738_tu1actual;
-pub mod v09730_10_04_09_1309_omaha_delta;
+pub mod v11883_10_10_25_1227_dlc_1_ship;
 pub mod v11860_10_07_24_0147_omaha_release;
+pub mod v09730_10_04_09_1309_omaha_delta;
+pub mod v09664_10_04_06_2121_omaha_beta;
 pub mod v09449_10_03_25_1545_omaha_beta;
 pub mod v08516_10_02_19_1607_omaha_alpha;
-pub mod v09664_10_04_06_2121_omaha_beta;
 
 pub const k_title_haloreach: &str = "Halo: Reach";
