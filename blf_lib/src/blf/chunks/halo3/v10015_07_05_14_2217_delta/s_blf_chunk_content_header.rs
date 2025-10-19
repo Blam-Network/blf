66cc87ab5fa987ebc7fd31948a3fea4262fc3b1b
Codie Newark <a13x231mcsl@gmail.com>
2025-10-19 14:39:57 +0900

    disabled chdr 9.1 napi support

--- blf_lib/src/blf/chunks/halo3/v10015_07_05_14_2217_delta/s_blf_chunk_content_header.rs
+++ blf_lib/src/blf/chunks/halo3/v10015_07_05_14_2217_delta/s_blf_chunk_content_header.rs
@@ -1,6 +1,6 @@
 use binrw::binrw;
-#[cfg(feature = "napi")]
-use napi_derive::napi;
+// #[cfg(feature = "napi")]
+// use napi_derive::napi;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::wasm_bindgen;
 use blf_lib::blam::halo3::v12070_08_09_05_2031_halo3_ship::saved_games::saved_game_files::s_content_item_metadata;
@@ -8,12 +8,13 @@ use blf_lib_derivable::blf::chunks::BlfChunkHooks;
 use blf_lib_derive::BlfChunk;
 
 #[binrw]
-#[cfg_attr(feature = "napi", napi(object, namespace = "halo3_10015_07_05_14_2217_delta"))]
+// Napi-rs is causing build issues with this chunk. We don't need it yet so it's been disabled.
+// #[cfg_attr(feature = "napi", napi(object, namespace = "halo3_10015_07_05_14_2217_delta"))]
 #[derive(BlfChunk,Default,PartialEq,Debug,Clone,Serialize,Deserialize)]
 #[Size(0xFC)]
 #[Header("chdr", 9.1)]
 #[brw(big)]
-#[wasm_bindgen(getter_with_clone)]
+// #[wasm_bindgen(getter_with_clone)]
 pub struct s_blf_chunk_content_header
 {
     pub build_number: u16,
