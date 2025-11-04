b3304d086faaffa39d3ae5d89b15e0153363d7c1
Codie Newark <a13x231mcsl@gmail.com>
2025-11-04 21:04:16 +0900

    fixed napi type for umsg

--- blf_lib/src/blf/chunks/haloreach/v12065_11_08_24_1738_tu1actual/s_blf_chunk_user_messaging_data.rs
+++ blf_lib/src/blf/chunks/haloreach/v12065_11_08_24_1738_tu1actual/s_blf_chunk_user_messaging_data.rs
@@ -5,6 +5,7 @@ use num_derive::FromPrimitive;
 use serde::{Deserialize, Serialize};
 use blf_lib::types::bool::Bool;
 use blf_lib::types::time::time64_t;
+use blf_lib::types::u64::Unsigned64;
 use blf_lib_derivable::blf::chunks::BlfChunkHooks;
 use blf_lib_derive::BlfChunk;
 use crate::types::array::StaticArray;
@@ -17,8 +18,8 @@ use crate::types::c_string::StaticString;
 #[Size(0xC)]
 #[cfg_attr(feature = "napi", napi(object, namespace = "haloreach_12065_11_08_24_1738_tu1actual"))]
 pub struct s_blf_chunk_user_messaging_data {
-    pub unknown0: u64, // might be unused.
-    pub message_index: u64,
+    pub unknown0: Unsigned64, // might be unused.
+    pub message_index: Unsigned64,
     pub expires_at: time64_t,
 }
 
