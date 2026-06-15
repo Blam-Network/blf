23f7c08a60ac1fc85fe6ee2ca5e129314307fdd1
Codie Newark <git@codie.gg>
2026-06-15 20:59:34 +0900

    c_megalo_game_statistic enums

--- blf-ts/src/blam/haloreach/v12065_11_08_24_1738_tu1actual/game/megalogamengine/c_megalo_game_statistic.ts
+++ blf-ts/src/blam/haloreach/v12065_11_08_24_1738_tu1actual/game/megalogamengine/c_megalo_game_statistic.ts
@@ -3,25 +3,62 @@ import type {
   c_bitstream_writer,
 } from "../../../../../bitstream";
 import { AutoMap } from "../../../../../helpers/automap";
+
+/** Matches `e_megalo_game_statistic_format` in blf_lib `megalogamengine_statistics.rs`. */
+export enum e_megalo_game_statistic_format {
+  number = 0,
+  number_with_sign = 1,
+  percentage = 2,
+  time = 3,
+}
+
+/** Matches `e_megalo_game_statistic_sort_order` in blf_lib `megalogamengine_statistics.rs`. */
+export enum e_megalo_game_statistic_sort_order {
+  none = -1,
+  ascending = 0,
+  descending = 1,
+}
+
+/** Matches `e_megalo_game_statistic_grouping` in blf_lib `megalogamengine_statistics.rs`. */
+export enum e_megalo_game_statistic_grouping {
+  player = 0,
+  team = 1,
+}
+
 export class c_megalo_game_statistic {
   @AutoMap(() => Number)
   m_name_string_index = 0;
-  @AutoMap(() => Number)
-  m_format = 0;
-  @AutoMap(() => Number)
-  m_sort_order = 0;
-  @AutoMap(() => Number)
-  m_growuping = 0;
+  @AutoMap(() => e_megalo_game_statistic_format)
+  m_format: e_megalo_game_statistic_format =
+    e_megalo_game_statistic_format.number;
+  @AutoMap(() => e_megalo_game_statistic_sort_order)
+  m_sort_order: e_megalo_game_statistic_sort_order =
+    e_megalo_game_statistic_sort_order.none;
+  @AutoMap(() => e_megalo_game_statistic_grouping)
+  m_growuping: e_megalo_game_statistic_grouping =
+    e_megalo_game_statistic_grouping.player;
   decode(bitstream: c_bitstream_reader): void {
     this.m_name_string_index = bitstream.read_integer("name-string-index", 7);
-    this.m_format = bitstream.read_integer("format", 2);
-    this.m_sort_order = bitstream.read_integer("sort-order", 2);
-    this.m_growuping = bitstream.read_integer("groupingt", 1);
+    this.m_format = bitstream.read_enum(
+      "format",
+      2,
+      e_megalo_game_statistic_format
+    );
+    this.m_sort_order = bitstream.read_enum(
+      "sort-order",
+      2,
+      e_megalo_game_statistic_sort_order
+    );
+    this.m_growuping = bitstream.read_enum(
+      "grouping",
+      1,
+      e_megalo_game_statistic_grouping
+    );
   }
   encode(bitstream: c_bitstream_writer): void {
     bitstream.write_integer(this.m_name_string_index, 7);
-    bitstream.write_integer(this.m_format, 2);
-    bitstream.write_integer(this.m_sort_order, 2);
-    bitstream.write_integer(this.m_growuping, 1);
+    bitstream.write_enum(this.m_format, 2, e_megalo_game_statistic_format);
+    bitstream.write_enum(this.m_sort_order, 2, e_megalo_game_statistic_sort_order);
+    bitstream.write_enum(this.m_growuping, 1, e_megalo_game_statistic_grouping);
   }
 }
